4a2806496a62facced37b0b36aadf08f2a86f062
John DoeRaphsodyz <arthurvsr@gmail.com>
2022-05-07 22:21:50 -0300

    modal inicio

--- NetDeliveryAppWeb/src/App.vue
+++ NetDeliveryAppWeb/src/App.vue
@@ -29,83 +29,19 @@
                         <router-link to="/Cadastro">Cadastro</router-link>
                     </li>
                 </ul>
-                <ul class="icons">
-                    <li>
-                        <a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a>
-                    </li>
-                    <li>
-                        <a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a>
-                    </li>
-                    <li>
-                        <a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a>
-                    </li>
-                    <li>
-                        <a href="#" class="icon brands fa-github"><span class="label">GitHub</span></a>
-                    </li>
-                </ul>
             </nav>
             <div id="main">
                 <router-view />
             </div>
             <footer id="footer">
-                <section>
-                    <form method="post" action="#">
-                        <div class="fields">
-                            <div class="field">
-                                <label for="name">Name</label>
-                                <input type="text" name="name" id="name" />
-                            </div>
-                            <div class="field">
-                                <label for="email">Email</label>
-                                <input type="text" name="email" id="email" />
-                            </div>
-                            <div class="field">
-                                <label for="message">Message</label>
-                                <textarea name="message" id="message" rows="3"></textarea>
-                            </div>
-                        </div>
-                        <ul class="actions">
-                            <li><input type="submit" value="Send Message" /></li>
-                        </ul>
-                    </form>
-                </section>
-                <section class="split contact">
-                    <section class="alt">
-                        <h3>Address</h3>
-                        <p>
-                            1234 Somewhere Road #87257<br />
-                            Nashville, TN 00000-0000
-                        </p>
-                    </section>
-                    <section>
-                        <h3>Phone</h3>
-                        <p><a href="#">(000) 000-0000</a></p>
-                    </section>
-                    <section>
-                        <h3>Email</h3>
-                        <p><a href="#">info@untitled.tld</a></p>
-                    </section>
-                    <section>
-                        <h3>Social</h3>
-                        <ul class="icons alt">
-                            <li>
-                                <a href="#" class="icon brands alt fa-twitter"><span class="label">Twitter</span></a>
-                            </li>
-                            <li>
-                                <a href="#" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a>
-                            </li>
-                            <li>
-                                <a href="#" class="icon brands alt fa-instagram"><span class="label">Instagram</span></a>
-                            </li>
-                            <li>
-                                <a href="#" class="icon brands alt fa-github"><span class="label">GitHub</span></a>
-                            </li>
-                        </ul>
-                    </section>
-                </section>
+                <ul class="icons">
+                    <li><a href="https://twitter.com/" target="_blank" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+                    <li><a href="https://www.facebook.com/" target="_blank" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+                    <li><a href="https://github.com/Raphsodyz" target="_blank" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
+                    <li><a href="https://www.linkedin.com/in/arthur-vin%C3%ADcius-souza-ribeiro-7bbb07107/" target="_blank" class="icon brands fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
+                    <li><a href="" class="icon solid fa-toolbox"><span class="label">Administrador</span></a></li>
+                </ul>
             </footer>
-
-            <!-- Copyright -->
             <div id="copyright">
                 <ul>
                     <li>&copy; Untitled</li>
@@ -121,11 +57,10 @@
     @import "../public/css/noscript.css";
     @import url(../public/css/fontawesome-all.min.css);
     @import url("https://fonts.googleapis.com/css?family=Merriweather:300,700,300italic,700italic|Source+Sans+Pro:900");
-    @import url("https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css");
 </style>
 
 <script>
-import Index from "./views/Index.vue";
+    import Index from "./views/Index.vue";
 
 export default {
   name: "App",
--- NetDeliveryAppWeb/src/views/Index.vue
+++ NetDeliveryAppWeb/src/views/Index.vue
@@ -4,13 +4,25 @@
             Um momento, as informações estão sendo carregadas...
         </div>
         <div v-if="post" class="content">
+            <div>
+                <b-modal centered
+                         id="modal-1"
+                         size="lg"
+                         title-tag="h2"
+                         no-close-on-backdrop
+                         :title="modal.nome">
+                    <b-img-lazy :src="modal.foto" fluid-grow :alt="modal.nome" />
+                    <p class="my-4">{{modal.ingredientes}}</p>
+                    <b-form></b-form>
+                </b-modal>
+            </div>
             <h1>Hamburguers</h1>
             <div id="features-wrapper">
                 <div class="container">
                     <div class="row">
                         <div class="col-4 col-12-medium" v-for="produtos in categoria1" :key="produtos.id">
                             <section class="box feature">
-                                <a href="#" class="image featured"><img :src="produtos.foto" alt="" /></a>
+                                <a v-b-modal.modal-1 @click="capturar(produtos.id)" class="image featured"><b-img-lazy :src="produtos.foto" :alt="modal.nome" /></a>
                                 <div class="inner">
                                     <header>
                                         <h2>{{produtos.nome}}</h2>
@@ -23,20 +35,20 @@
                         <h1>Bebidas</h1>
                         <div class="col-4 col-12-medium" v-for="produtos in categoria2" :key="produtos.id">
                             <section class="box feature">
-                                <a href="#" class="image featured"><img :src="produtos.foto" alt="" /></a>
+                                <a v-b-modal.modal-1 @click="capturar(produtos.id)" class="image featured"><b-img-lazy :src="produtos.foto" :alt="modal.nome" /></a>
                                 <div class="inner">
                                     <header>
                                         <h2>{{produtos.nome}}</h2>
                                         <h3>R${{produtos.valor}}</h3>
                                     </header>
-                                    <p>{{produtos.ingredientes}}</p>
+                                    <p class="card-text">{{produtos.ingredientes}}</p>
                                 </div>
                             </section>
                         </div>
                         <h1>Sucos</h1>
                         <div class="col-4 col-12-medium" v-for="produtos in categoria3" :key="produtos.id">
                             <section class="box feature">
-                                <a href="#" class="image featured"><img :src="produtos.foto" alt="" /></a>
+                                <a v-b-modal.modal-1 @click="capturar(produtos.id)" class="image featured"><b-img-lazy :src="produtos.foto" :alt="modal.nome" /></a>
                                 <div class="inner">
                                     <header>
                                         <h2>{{produtos.nome}}</h2>
@@ -49,7 +61,7 @@
                         <h1>Cervejas</h1>
                         <div class="col-4 col-12-medium" v-for="produtos in categoria4" :key="produtos.id">
                             <section class="box feature">
-                                <a href="#" class="image featured"><img :src="produtos.foto" alt="" /></a>
+                                <a v-b-modal.modal-1 @click="capturar(produtos.id)" class="image featured"><b-img-lazy :src="produtos.foto" :alt="modal.nome" /></a>
                                 <div class="inner">
                                     <header>
                                         <h2>{{produtos.nome}}</h2>
@@ -67,7 +79,9 @@
 </template>
 
 <script lang="js">
-    import Vue from 'vue';
+    
+import axios from 'axios';
+import Vue from 'vue';
 
     export default
         Vue.extend({
@@ -75,6 +89,7 @@
                 return {
                     loading: false,
                     post: null,
+                    modal:{},
                 }
             },
             created() {
@@ -95,6 +110,16 @@
                             this.post = json;
                             this.loading = false;
                             return;
+                        });
+                },
+                capturar(id) {
+                    var selecionado = id;
+                    axios.get('https://localhost:5001/api/Produtos/Encontrar/' + selecionado.toString())
+                        .then((resposta) => {
+                            this.modal = resposta.data
+                        })
+                        .catch((error) => {
+                            console.log(error);
                         });
                 }
             },
