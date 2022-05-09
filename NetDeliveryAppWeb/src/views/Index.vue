0957dd134bfbc12635655f63259c7d31ada3160e
John DoeRaphsodyz <arthurvsr@gmail.com>
2022-05-08 23:37:40 -0300

    inicio carrinho

--- NetDeliveryAppWeb/src/views/Index.vue
+++ NetDeliveryAppWeb/src/views/Index.vue
@@ -4,18 +4,6 @@
             Um momento, as informações estão sendo carregadas...
         </div>
         <div v-if="post" class="content">
-            <div>
-                <b-modal centered
-                         id="modal-1"
-                         size="lg"
-                         title-tag="h2"
-                         no-close-on-backdrop
-                         :title="modal.nome">
-                    <b-img-lazy :src="modal.foto" fluid-grow :alt="modal.nome" />
-                    <p class="my-4">{{modal.ingredientes}}</p>
-                    <b-form></b-form>
-                </b-modal>
-            </div>
             <h1>Hamburguers</h1>
             <div id="features-wrapper">
                 <div class="container">
@@ -25,10 +13,11 @@
                                 <a v-b-modal.modal-1 @click="capturar(produtos.id)" class="image featured"><b-img-lazy :src="produtos.foto" :alt="modal.nome" /></a>
                                 <div class="inner">
                                     <header>
-                                        <h2>{{produtos.nome}}</h2>
-                                        <h3>R${{produtos.valor}}</h3>
+                                        <h2 class="titulo">{{produtos.nome}}</h2>
+                                        <p class="card-text">{{produtos.ingredientes}}</p>
+                                        <br />
+                                        <h2 class="produto-valor">R${{produtos.valor}}</h2>
                                     </header>
-                                    <p>{{produtos.ingredientes}}</p>
                                 </div>
                             </section>
                         </div>
@@ -38,10 +27,11 @@
                                 <a v-b-modal.modal-1 @click="capturar(produtos.id)" class="image featured"><b-img-lazy :src="produtos.foto" :alt="modal.nome" /></a>
                                 <div class="inner">
                                     <header>
-                                        <h2>{{produtos.nome}}</h2>
-                                        <h3>R${{produtos.valor}}</h3>
+                                        <h2 class="titulo">{{produtos.nome}}</h2>
+                                        <p class="card-text">{{produtos.ingredientes}}</p>
+                                        <br />
+                                        <h2 class="produto-valor">R${{produtos.valor}}</h2>
                                     </header>
-                                    <p class="card-text">{{produtos.ingredientes}}</p>
                                 </div>
                             </section>
                         </div>
@@ -51,10 +41,11 @@
                                 <a v-b-modal.modal-1 @click="capturar(produtos.id)" class="image featured"><b-img-lazy :src="produtos.foto" :alt="modal.nome" /></a>
                                 <div class="inner">
                                     <header>
-                                        <h2>{{produtos.nome}}</h2>
-                                        <h3>R${{produtos.valor}}</h3>
+                                        <h2 class="titulo">{{produtos.nome}}</h2>
+                                        <p class="card-text">{{produtos.ingredientes}}</p>
+                                        <br />
+                                        <h2 class="produto-valor">R${{produtos.valor}}</h2>
                                     </header>
-                                    <p>{{produtos.ingredientes}}</p>
                                 </div>
                             </section>
                         </div>
@@ -64,24 +55,103 @@
                                 <a v-b-modal.modal-1 @click="capturar(produtos.id)" class="image featured"><b-img-lazy :src="produtos.foto" :alt="modal.nome" /></a>
                                 <div class="inner">
                                     <header>
-                                        <h2>{{produtos.nome}}</h2>
-                                        <h3>R${{produtos.valor}}</h3>
+                                        <h2 class="titulo">{{produtos.nome}}</h2>
+                                        <p class="card-text">{{produtos.ingredientes}}</p>
+                                        <br />
+                                        <h2 class="produto-valor">R${{produtos.valor}}</h2>
                                     </header>
-                                    <p>{{produtos.ingredientes}}</p>
                                 </div>
                             </section>
                         </div>
                     </div>
                 </div>
             </div>
+            <div>
+                <b-modal centered
+                         id="modal-1"
+                         title-tag="h2"
+                         body-class="b-row"
+                         no-close-on-backdrop
+                         :title="modal.nome">
+                    <b-img-lazy :src="modal.foto" fluid-grow :alt="modal.nome" />
+                    <p class="my-4" style="text-align: center;
+                                        font-family: 'PT Sans', sans-serif;
+                                        font-size: medium;">
+                        {{modal.ingredientes}}
+                    </p>
+
+                    <b-form @submit="carrinho" v-if="show">
+                        <b-form-group id="entrada1"
+                                      label="Lanche"
+                                      label-for="lanche">
+                            <b-form-input id="lanche"
+                                          v-model="carrinho.lanche"
+                                          type="hidden"
+                                          placeholder="Nome do lanche">
+                                {{modal.nome}}
+                            </b-form-input>
+                        </b-form-group>
+
+                        <b-form-group id="entrada2"
+                                      label="Valor do lanche"
+                                      label-for="valorLanche">
+                            <b-form-input id="valorLanche"
+                                          v-model="carrinho.valorLanche"
+                                          type="hidden"
+                                          placeholder="Valor do lanche">
+                                {{modal.valor}}
+                            </b-form-input>
+                        </b-form-group>
+
+                        <b-form-group id="entrada3"
+                                      label="Alguma observação sobre o lanche?"
+                                      label-for="observacao">
+                            <b-form-input id="observacao"
+                                          v-model="carrinho.observacao"
+                                          type="textarea"
+                                          placeholder="Digite aqui!"></b-form-input>
+                        </b-form-group>
+
+                        <b-form-group id="entrada4"
+                                      label="Quantidade?"
+                                      label-for="quantidade"
+                                      description="Se vazio, entenderemos que é somente um.">
+                            <b-form-input id="quantidade"
+                                          v-model="carrinho.quantidade"
+                                          type="number"></b-form-input>
+                        </b-form-group>
+
+                        <b-form-group id="entrada5"
+                                      v-slot="{ ariaDescribedby }">
+                            <b-form-checkbox-group id="acrescimos"
+                                                   v-model="carrinho.acrescimosValor"
+                                                   :aria-describedby="ariaDescribedby"
+                                                   v-for="acrescimo in acrescimosLista"
+                                                   :key="acrescimo.id">
+                                <b-form-checkbox :value="acrescimo.valor">
+                                    {{acrescimo.nome}} R${{acrescimo.valor}}
+                                </b-form-checkbox>
+                            </b-form-checkbox-group>
+                        </b-form-group>
+
+                        <template #modal-footer>
+                            <div class="w-100">
+                                <b-button block
+                                          variant="dark"
+                                          type="submit">Adicionar ao carrinho</b-button>
+                            </div>
+                        </template>
+                    </b-form>
+                </b-modal>
+            </div>
         </div>
     </div>
 </template>
 
 <script lang="js">
-    
-import axios from 'axios';
-import Vue from 'vue';
+
+    import axios from 'axios';
+    import Vue from 'vue';
 
     export default
         Vue.extend({
@@ -89,7 +159,17 @@ import Vue from 'vue';
                 return {
                     loading: false,
                     post: null,
-                    modal:{},
+                    modal: {},
+                    acrescimosLista: null,
+                    carrinho: {
+                        lanche: '',
+                        valorLanche: '',
+                        observacao: '',
+                        quantidade: '',
+                        acrescimosValor: [],
+                        total: parseInt(this.quantidade) * (parseFloat(this.valorLanche) + (this.acrescimosValor.reduce((anterior, atual) => anterior + atual, 0)))
+                    },
+                    show: true,
                 }
             },
             created() {
@@ -121,6 +201,20 @@ import Vue from 'vue';
                         .catch((error) => {
                             console.log(error);
                         });
+                },
+                getAcrescimos() {
+                    axios.get('https://localhost:5001/api/Acrescimos/Listar')
+                        .then((resposta) => {
+                            this.acrescimosLista = resposta.data
+                        })
+                        .catch((error) => {
+                            console.log(error);
+                        })
+                },
+                submit(event) {
+                    event.preventDefault();
+                    alert(JSON.stringify(this.carrinho));
+                    console.log(this.carrinho);
                 }
             },
             computed: {
